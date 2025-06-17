f4d7b4616fe1bc3f9694cad38e730dda420eaaca
Noah Grayson <124320422+noah-sec@users.noreply.github.com>
2025-06-17 08:58:12 -0700

    Update WN10-00-000090.ps1

--- WN10-00-000090.ps1
+++ WN10-00-000090.ps1
@@ -1,3 +1,15 @@
+<#
+.SYNOPSIS
+    This PowerShell script sets the maximum password age for user accounts to 90 days.
+    
+.NOTES
+    Author          : Noah Grayson
+    STIG-ID         : WN10-00-000090 
+    
+.EXAMPLE
+    .\WN10-00-000090.ps1
+#>
 #Requires -RunAsAdministrator
+
 # This command sets the maximum password age for user accounts on the local system to 90 days.
 net accounts /maxpwage:90
